34b946a50883265db5403f936403a6af8cbda4dd
mikhaelarya <5025211062@mhs.its.ac.id>
2023-11-05 14:52:04 +0700

    add room_images table

---
+++ database/migrations/2023_11_05_074648_create_room_images_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('room_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_path');
+            $table->unsignedBigInteger('room_id');
+
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room_images');
+    }
+};
