ce84e8d06cd7eeddd398fbc20f6ccea24c65cdab
riskiilyas <riskiilyas03@gmail.com>
2023-11-09 10:37:44 +0700

    add: bank payment to booking page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,39 +18,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-//        // Creating Users
-//        \App\Models\User::factory(50)->create();
-//
-//        // Creating Room Types
-//        \App\Models\RoomType::factory()->create([
-//            'name' => 'Suite',
-//            'price' => 800_000
-//        ]);
-//        \App\Models\RoomType::factory()->create([
-//            'name' => 'Family Room',
-//            'price' => 1_500_000
-//        ]);
-//        \App\Models\RoomType::factory()->create([
-//            'name' => 'Presidential Suite',
-//            'price' => 8_000_000
-//        ]);
-//        \App\Models\RoomType::factory()->create([
-//            'name' => 'Deluxe Room',
-//            'price' => 4_000_000
-//        ]);
-//        \App\Models\RoomType::factory()->create([
-//            'name' => 'Basic Room',
-//            'price' => 350_000
-//        ]);
-//
-//        BranchAddress::factory(20)->create();
-//
-//        // Creating the Rooms
-//        Room::factory(250)->create();
-//
-//        $imageEachRoom = 3;
-//        for ($i=0; $i < $imageEachRoom; $i++) $this->createRoomImageEachRooms();
+        // Creating Users
+        \App\Models\User::factory(50)->create();
 
+        // Creating Room Types
+        \App\Models\RoomType::factory()->create([
+            'name' => 'Suite',
+            'price' => 800_000
+        ]);
+        \App\Models\RoomType::factory()->create([
+            'name' => 'Family Room',
+            'price' => 1_500_000
+        ]);
+        \App\Models\RoomType::factory()->create([
+            'name' => 'Presidential Suite',
+            'price' => 8_000_000
+        ]);
+        \App\Models\RoomType::factory()->create([
+            'name' => 'Deluxe Room',
+            'price' => 4_000_000
+        ]);
+        \App\Models\RoomType::factory()->create([
+            'name' => 'Basic Room',
+            'price' => 350_000
+        ]);
+
+        // Creating Branch Addresses
+        BranchAddress::factory(20)->create();
+
+        // Creating the Rooms
+        Room::factory(250)->create();
+
+        // Creating 3 Images for Each Room
+        $imageEachRoom = 3;
+        for ($i=0; $i < $imageEachRoom; $i++) $this->createRoomImageEachRooms();
+
+
+        // Creating Payment Banks
         $bankNames = ['Bank BCA', 'Bank BNI', 'Bank BRI', 'Bank BTN', 'Bank BTPN', 'Bank Mandiri', 'Bank MEGA', 'Bank Muamalat'
             , 'Bank Sinarmas', 'Bank Syariah Mandiri', 'BTN Syariah', 'CIMB Niaga', 'Citibank', 'Danamon', 'OCBC NISP', 'Permata Bank'
         ];
--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -21,7 +21,7 @@
 
     <div class="py-12">
         <div class="bg-white max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden flex items-center">
+            <div class="overflow-hidden flex">
                 <div class="container mx-auto p-4 w-1/2">
                     <!-- Information section -->
                     <h1 class="text-2xl font-semibold mb-4">Upload Proof of Payment</h1>
@@ -39,55 +39,88 @@
                             @endif
                         </h2>
                     </div>
-                    <p class="mb-4 font-medium text-gray-700">Bank Account for Transfer:</p>
-                    <select name="payment_bank_id" id="payment_bank_id" class="form-control">
-                        <option value="">Select a Payment Bank</option>
-                        @foreach($paymentBanks as $paymentBank)
-                            <option value="{{ $paymentBank->id }}">
-                                {{ $paymentBank->bank_name }}
-                            </option>
-                        @endforeach
-                    </select>
-                    <br>
-                    <img id="bankImage" src="" alt="Selected Bank Image" style="max-width: 200px; max-height: 200px; display: none; margin-top: 10px;">
-                    <p><strong>Bank Name:</strong> <span id="bankName"></span></p>
-                    <p><strong>Account Name:</strong> <span id="accountName"></span></p>
-                    <p><strong>Account Number:</strong> <span id="accountNumber"></span></p>
 
-                    <script>
-                        // Add JavaScript to handle the displayed information based on the selected bank
-                        const paymentBankSelect = document.getElementById('payment_bank_id');
-                        const bankNameElement = document.getElementById('bankName');
-                        const accountNameElement = document.getElementById('accountName');
-                        const accountNumberElement = document.getElementById('accountNumber');
-                        const bankImage = document.getElementById('bankImage');
+                    <form
+                        @if(isset($parameters['daterange']))
+                            action="/pay/{{$room->id}}?daterange={{$parameters['daterange']}}"
+                        @else
+                            action="{{ route('pay', ['id' => $room->id]) }}"
+                        @endif
+                        method="post" enctype="multipart/form-data" class="container mx-auto p-4 w-full w-md-1/2"
+                        id="paymentForm">
+                        @csrf
+                        <p class="mb-4 font-medium text-gray-700">Bank Account for Transfer:</p>
+                        <select name="payment_bank_id" id="payment_bank_id" class="form-control">
+                            <option value="">Select a Payment Bank</option>
+                            @foreach($paymentBanks as $paymentBank)
+                                <option value="{{ $paymentBank->id }}">
+                                    {{ $paymentBank->bank_name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <br>
+                        <img id="bankImage" src="" alt="Selected Bank Image"
+                             style="max-width: 200px; max-height: 200px; display: none; margin-top: 10px;">
+                        <p><strong>Bank Name:</strong> <span id="bankName">-</span></p>
+                        <p><strong>Account Name:</strong> <span id="accountName">-</span></p>
+                        <p><strong>Account Number:</strong> <span id="accountNumber">-</span></p>
 
-                        paymentBankSelect.addEventListener('change', function () {
-                            const selectedOption = paymentBankSelect.options[paymentBankSelect.selectedIndex];
-                            if (selectedOption.value) {
-                                const selectedBankId = selectedOption.value;
-                                const selectedPaymentBank = @json($paymentBanks->keyBy('id'));
-                                const selectedBank = selectedPaymentBank[selectedOption.value];
-                                // Update the displayed information with the retrieved bank details
-                                bankNameElement.textContent = data.bankName;
-                                accountNameElement.textContent = data.accountName;
-                                accountNumberElement.textContent = data.accountNumber;
-                                bankImage.src = `{{ asset('storage/') }}/${selectedBank.bank_image_path}`;
-                                bankImage.style.display = 'block';
-                            } else {
-                                // Clear the displayed information when no bank is selected
-                                bankNameElement.textContent = '';
-                                accountNameElement.textContent = '';
-                                accountNumberElement.textContent = '';
-                            }
-                        });
-                    </script>
+                        <script>
+                            // Add JavaScript to handle the displayed information based on the selected bank
+                            const paymentBankSelect = document.getElementById('payment_bank_id');
+                            const bankNameElement = document.getElementById('bankName');
+                            const accountNameElement = document.getElementById('accountName');
+                            const accountNumberElement = document.getElementById('accountNumber');
+                            const bankImage = document.getElementById('bankImage');
+
+                            paymentBankSelect.addEventListener('change', function () {
+                                const selectedOption = paymentBankSelect.options[paymentBankSelect.selectedIndex];
+                                if (selectedOption.value) {
+                                    const selectedPaymentBank = @json($paymentBanks->keyBy('id'));
+                                    const selectedBank = selectedPaymentBank[selectedOption.value];
+                                    bankImage.src = `{{ asset('storage') }}/${selectedBank.bank_image_path}`;
+                                    bankImage.style.display = 'block';
+                                    bankNameElement.textContent = selectedBank.bank_name;
+                                    accountNameElement.textContent = selectedBank.account_name;
+                                    accountNumberElement.textContent = selectedBank.bank_account;
+                                } else {
+                                    // Clear the displayed information when no bank is selected
+                                    bankNameElement.textContent = '-';
+                                    accountNameElement.textContent = '-';
+                                    accountNumberElement.textContent = '-';
+                                    bankImage.style.display = 'none';
+                                    bankImage.src = '';
+                                }
+                            });
+                        </script>
+
+                        <br>
+                        <div class="mb-4 overflow-hidden flex flex-col">
+                            <!-- Center the label and input vertically -->
+                            <label for="payment_proof" class="block font-medium text-gray-700 mb-2">Payment
+                                Proof</label>
+                            <div class="flex"> <!-- Center the displayed image horizontally -->
+                                <img id="selectedImage" src="" alt="Selected Image" class="mt-4"
+                                     style="max-width: 100%; max-height: 200px; display: none; margin: auto; border-radius: 10px;">
+                            </div>
+                            <br>
+                            <div class="flex">
+                                <input type="file" name="payment_proof" id="payment_proof"
+                                       accept=".png, .jpg, .jpeg"
+                                       class="border rounded w-full p-2 py-3" onchange="displayImage(this);">
+                            </div>
+                        </div>
+                        <div class="flex"> <!-- Center the button horizontally -->
+                            <x-primary-button>{{ __('Submit Payment') }}</x-primary-button>
+                        </div>
+                    </form>
+
                 </div>
 
-                <div class="w-1/2" style="height: 300px; max-width: 500px;">
+                <div class="w-1/2" style="max-width: 500px; display: flex; flex-direction: column;">
                     <!-- Image Carousel -->
-                    <div style="padding: 20px;"> <!-- Adjust the padding value as needed -->
-                        <div class="owl-carousel owl-theme">
+                    <div style="padding: 20px; flex: 1;">
+                        <div class="owl-carousel owl-theme" style="flex: 1;">
                             @foreach($room->images as $image)
                                 <div class="item">
                                     <a href="{{ asset('storage/' . $image->image_path) }}" data-lightbox="room-images">
@@ -99,40 +132,16 @@
                         </div>
                     </div>
                 </div>
+
+
+
             </div>
-            <br>
-
-            <!-- Form section -->
-            <form
-                @if(isset($parameters['daterange']))
-                    action="/pay/{{$room->id}}?daterange={{$parameters['daterange']}}"
-                @else
-                    action="{{ route('pay', ['id' => $room->id]) }}"
-                @endif
-                method="post" enctype="multipart/form-data" class="container mx-auto p-4 w-1/2" id="paymentForm">
-                @csrf
-
-                <div class="mb-4 overflow-hidden items-center flex flex-col"> <!-- Center the label and input vertically -->
-                    <label for="payment_proof" class="block font-medium text-gray-700 text-center mb-2">Payment Proof</label>
-                    <div class="flex justify-center"> <!-- Center the displayed image horizontally -->
-                        <img id="selectedImage" src="" alt="Selected Image" class="mt-4" style="max-width: 100%; max-height: 200px; display: none; margin: auto; border-radius: 10px;">
-                    </div>
-                    <br>
-                    <div class="flex items-center justify-center">
-                        <input type="file" name="payment_proof" id="payment_proof" accept=".png, .jpg, .jpeg"
-                               class="border rounded w-full p-2 py-3" onchange="displayImage(this);">
-                    </div>
-                </div>
-                <div class="flex justify-center"> <!-- Center the button horizontally -->
-                    <x-primary-button>{{ __('Submit Payment') }}</x-primary-button>
-                </div>
-            </form>
             <script>
                 function displayImage(input) {
                     const selectedImage = document.getElementById('selectedImage');
                     if (input.files && input.files[0]) {
                         const reader = new FileReader();
-                        reader.onload = function(e) {
+                        reader.onload = function (e) {
                             selectedImage.style.display = 'block';
                             selectedImage.src = e.target.result;
                         };
