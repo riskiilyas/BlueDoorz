fbeb3090f2d713cc2a135745341b0f91004d53bb
riskiilyas <riskiilyas03@gmail.com>
2023-11-16 09:13:53 +0700

    change: email view

--- resources/views/mail/reservation_email.blade.php
+++ resources/views/mail/reservation_email.blade.php
@@ -1,4 +1,19 @@
-Dear {{$name}},
-Your Reservation for Room {{$roomNumber}} has already been created. Thank you for using BlueDoorz.
-
-Regards, BlueDoorz Team
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reservation Confirmation</title>
+    <style>
+        @import url('https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css');
+    </style>
+</head>
+<body class="bg-gray-100 p-4">
+<div class="max-w-md mx-auto bg-white p-8 rounded shadow-md">
+    <h2 class="text-2xl font-semibold mb-4">Reservation Confirmation</h2>
+    <p class="mb-4">Dear {{ $name }},</p>
+    <p>Your reservation for Room {{ $roomNumber }} has already been created. Thank you for choosing BlueDoorz.</p>
+    <p class="mt-4">Regards,<br>BlueDoorz Team</p>
+</div>
+</body>
+</html>
